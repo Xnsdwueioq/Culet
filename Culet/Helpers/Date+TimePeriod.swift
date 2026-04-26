3be6bd6237089d20ff1d6082db0acf60702b6a0f
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-04-27 01:35:01 +0700

    fix: decouple the timePeriod function from the current time

--- Culet/Helpers/Date+TimePeriod.swift
+++ Culet/Helpers/Date+TimePeriod.swift
@@ -10,20 +10,14 @@ import Foundation
 extension Date {
   /// Returns a TimePeriod for the date
   func timePeriod(relativeTo now: Date = .now) -> TimePeriod {
-    let calendar = Calendar.current
-    
-    if calendar.isDateInToday(self) {
-      return .today
-    }
-    
-    if calendar.isDateInYesterday(self) {
-      return .yesterday
-    }
-    
-    let components = calendar.dateComponents([.day], from: self, to: now)
+    let components = Calendar.current.dateComponents([.day], from: self, to: now)
     let days = components.day ?? 0
     
     switch days {
+    case 0:
+      return .today
+    case 1:
+      return .yesterday
     case 0...7:
       return .last7Days
     case 8...30:
