368d2e5b59027d6b6197d283fdbcf6b43aa9ac47
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-04-26 20:39:30 +0700

    feat: add Logger extension

---
+++ Culet/Helpers/Core/Logger.swift
@@ -0,0 +1,15 @@
+//
+//  Logger.swift
+//  Culet
+//
+//  Created by Eyhciurmrn Zmpodackrl on 26.04.2026.
+//
+
+import os
+import Foundation
+
+extension Logger {
+  private static var subsystem = Bundle.main.bundleIdentifier!
+  
+  static let ui = Logger(subsystem: subsystem, category: "UI")
+}
