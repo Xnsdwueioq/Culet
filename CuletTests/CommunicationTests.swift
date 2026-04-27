adcd1ca79c08a016b7a8aa09ccc8f09cb0d4119e
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-04-27 23:38:00 +0700

    chore: add test frame

---
+++ CuletTests/CommunicationTests.swift
@@ -0,0 +1,29 @@
+//
+//  CommunicationTests.swift
+//  Culet
+//
+//  Created by Eyhciurmrn Zmpodackrl on 27.04.2026.
+//
+
+import Foundation
+import Testing
+@testable import Culet
+
+@Suite("Тестирование сервиса для связи с пациентом")
+struct CommunicationTests {
+//  let sup = 
+  
+  @Test("Звонок пациенту", arguments: [
+    
+  ])
+  func call(_ phoneNumber: String, _ expected: Bool) {
+    // Arrange
+    
+    
+    // Act
+    
+    
+    // Assert
+    
+  }
+}
